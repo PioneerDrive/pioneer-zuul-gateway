e842b785527a66a6df7f77cb13f5be9eea89ef30
gonzalb11 <benedicto.gonzales@toyota.com>
2019-04-22 16:15:28 -0500

    Add config

--- src/main/java/com/pioneer/zuul/gateway/pioneerzuulgateway/PioneerZuulGatewayApplication.java
+++ src/main/java/com/pioneer/zuul/gateway/pioneerzuulgateway/PioneerZuulGatewayApplication.java
@@ -2,6 +2,7 @@ package com.pioneer.zuul.gateway.pioneerzuulgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
@@ -12,6 +13,7 @@ import com.pioneer.zuul.gateway.pioneerzuulgateway.filters.RouteFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 public class PioneerZuulGatewayApplication {
 
 	public static void main(String[] args) {
